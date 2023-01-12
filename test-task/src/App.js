649b410e934fa862775439c78e51d65e6e7b53b0
Dominik Stanczyk <stanczykdominik0@gmail.com>
2023-01-12 20:20:35 +0100

    Adding Product Add component

--- test-task/src/App.js
+++ test-task/src/App.js
@@ -1,13 +1,16 @@
 import styles from "./App.module.css";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import ProductAdd from "./components/productAdd/ProductAdd";
 import Tasks from "./components/tasks/Tasks";
 
+const test = false;
 function App() {
   return (
     <div className={styles.container}>
       <Header />
-      <Tasks />
+      {test && <Tasks />}
+      {!test && <ProductAdd />}
       <Footer />
     </div>
   );
---
+++ test-task/src/components/productAdd/ProductAdd.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./ProductAdd.module.css";
+
+function ProductAdd() {
+  return (
+    <div className={styles[""]}>
+      <form>
+        <label for="fname">SKU</label>
+        <input type="text" />
+        <br />
+        <label for="fname">Name</label>
+        <input type="text" />
+        <br />
+        <label for="fname">Price ($)</label>
+        <input type="number" />
+        <br />
+        <label for="fname">Type Switcher</label>
+        <select name="select" id="pet-select">
+          <option value="size">Size (in MB) for DVD-disc</option>
+          <option value="weight">Weight (in Kg) for Book</option>
+          <option value="dimensions">Dimensions (HxWxL) for Furniture</option>
+        </select>
+      </form>
+    </div>
+  );
+}
+
+export default ProductAdd;
