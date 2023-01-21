668a8e1514eb04e1acc538dc19770055aceb68d7
Dominik Stanczyk <stanczykdominik0@gmail.com>
2023-01-21 20:09:44 +0100

    adding "Type Switcher" functionality

--- test-task/src/components/productAdd/ProductAdd.js
+++ test-task/src/components/productAdd/ProductAdd.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProductAdd.module.css";
 
 function ProductAdd() {
+  const [option, setOption] = useState(0);
+
+  const size = (
+    <div>
+      {" "}
+      <label for="fname">Size (MB)</label>
+      <input type="number" />{" "}
+    </div>
+  );
+  const weight = (
+    <div>
+      {" "}
+      <label for="fname">Size (CM)</label>
+      <input type="number" />
+      <br />
+      <label for="fname">Width (CM)</label>
+      <input type="number" />
+      <br />
+      <label for="fname">Lenght (CM)</label>
+      <input type="number" />{" "}
+    </div>
+  );
+  const dimensions = (
+    <div>
+      {" "}
+      <label for="fname">Weight (KG)</label>
+      <input type="number" />{" "}
+    </div>
+  );
+
+  let selectedOption = "";
+
+  switch (option) {
+    case 0:
+      selectedOption = "";
+      break;
+    case 1:
+      selectedOption = size;
+      break;
+    case 2:
+      selectedOption = weight;
+      break;
+    case 3:
+      selectedOption = dimensions;
+      break;
+    default:
+      break;
+  }
+
+  const typeSwitcher = (e) => {
+    console.log(e.target.value);
+    switch (e.target.value) {
+      case "empty":
+        setOption(0);
+        break;
+      case "size":
+        setOption(1);
+        break;
+      case "weight":
+        setOption(2);
+        break;
+      case "dimensions":
+        setOption(3);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <div className={styles["container"]}>
       <form>
@@ -15,12 +84,13 @@ function ProductAdd() {
         <input type="number" />
         <br />
         <label for="fname">Type Switcher</label>
-        <select name="select" id="pet-select">
+        <select name="select" id="pet-select" onChange={typeSwitcher}>
           <option value="empty"></option>
           <option value="size">Size (in MB) for DVD-disc</option>
           <option value="weight">Weight (in Kg) for Book</option>
           <option value="dimensions">Dimensions (HxWxL) for Furniture</option>
         </select>
+        {selectedOption}
       </form>
     </div>
   );
